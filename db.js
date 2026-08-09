e1a181c9536c3f5ac5958344a8ccff164d7ae34a
Ralf85 <ralf.rogov@gmail.com>
2026-08-09 09:39:34 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,14 @@
-const { Pool } = require('pg'); // Railway annab DATABASE_URL keskkonnamuutuja automaatselt, kui projektile // on lisatud Postgres plugin. Railway sisemine (privaatvõrgu) andmebaasiühendus // ei vaja SSL-i - seetõttu jätame selle siin lihtsalt väljalülitatuks. const pool = new Pool({ connectionString: process.env.DATABASE_URL, }); pool.on('error', (err) => { console.error('Ootamatu viga andmebaasi connection pool\'is:', err); }); module.exports = pool;
+const { Pool } = require('pg');
+
+// Railway annab DATABASE_URL keskkonnamuutuja automaatselt, kui projektile
+// on lisatud Postgres plugin. Railway sisemine (privaatvõrgu) andmebaasiühendus
+// ei vaja SSL-i - seetõttu jätame selle siin lihtsalt väljalülitatuks.
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('error', (err) => {
+    console.error('Ootamatu viga andmebaasi connection pool\'is:', err);
+});
+
+module.exports = pool;
